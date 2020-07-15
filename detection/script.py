0c76fec3d724f9c40f8381b7fd2d9e30ebec0a2c
naivoder <naivoder@gmail.com>
2020-07-15 12:38:13 -0400

    work on object detection api

---
+++ detection/script.py
@@ -0,0 +1,8 @@
+import os
+from subprocess import check_output
+
+source = 'OID/Dataset/train'
+
+for folder in os.listdir(source):
+  target = f'{source}/{folder}'
+  output = check_output(["python3", 'OIDv4_annotation_tool/OIDv4_to_VOC.py', "--sourcepath" , f"{source}/{folder}", "--dest_path", f"{target}"])
